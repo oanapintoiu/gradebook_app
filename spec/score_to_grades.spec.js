fdd749cbd9450752c7808d9161dd750e47628fa3
Oana Pintoiu <oana.pintoiu@yahoo.com>
2024-05-14 16:50:10 +0100

    added grades A to F, all passing tests

--- spec/score_to_grades.spec.js
+++ spec/score_to_grades.spec.js
@@ -4,5 +4,15 @@ describe("getGrade function", () => {
   test("converts score to grade", () => {
     const grade = getGrade(100);
     expect(grade).toBe("A++");
+    const grade2 = getGrade(95);
+    expect(grade2).toBe("A");
+    const grade3 = getGrade(85);
+    expect(grade3).toBe("B");
+    const grade4 = getGrade(75);
+    expect(grade4).toBe("C");
+    const grade5 = getGrade(65);
+    expect(grade5).toBe("D");
+    const grade6 = getGrade(55);
+    expect(grade6).toBe("F");
   });
 });
--- src/score_to_grades.js
+++ src/score_to_grades.js
@@ -2,8 +2,21 @@ function getGrade(score) {
   let grade = "";
   if (score === 100) {
     grade = "A++";
+  } else if (score >= 90 && score <= 99) {
+    grade = "A";
+  } else if (score >= 80 && score <= 89) {
+    grade = "B";
+  } else if (score >= 70 && score <= 79) {
+    grade = "C";
+  } else if (score >= 60 && score <= 69) {
+    grade = "D";
+  } else {
+    grade = "F";
   }
   return grade;
 }
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
 
 module.exports = getGrade;
