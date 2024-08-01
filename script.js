65fce947527455ae85a5996bdc1f38840f873ec4
Oana Pintoiu <oana.pintoiu@yahoo.com>
2024-08-01 17:12:22 +0100

    created a main function that will hold multiple variables

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const readline = require('readline');
+const { getGrade, getPassingGrade } = require('./src/score_to_grades');
+const getAverage = require('./src/average_score');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,25 +11,22 @@ function askQuestion(query) {
   return new Promise(resolve => rl.question(query, resolve));
 }
 
-async function getValidNumberOfGrades() {
-  while (true) {
-    const input = await askQuestion('Enter the number of grades (max 6): ');
-    const numGrades = parseInt(input, 10);
-
-    if (numGrades >= 1 && numGrades <= 6) {
-      return numGrades;
-    } else {
-      console.log('Please enter a number between 1 and 6.');
-    }
-  }
-}
-
 async function main() {
-  const numGrades = await getValidNumberOfGrades();
-  console.log(`Number of grades entered: ${numGrades}`);
+  const numGrades = parseInt(await askQuestion('Enter the number of grades (max 6): '), 10);
+
+  if (numGrades < 1 || numGrades > 6) {
+    console.log('Please enter a number between 1 and 6.');
+    rl.close();
+    return;
+  }
+  
+  const message = `Your number of grades is ${numGrades}`;
+
+  console.log(message);
   rl.close();
 }
 
 main();
+
 
 
